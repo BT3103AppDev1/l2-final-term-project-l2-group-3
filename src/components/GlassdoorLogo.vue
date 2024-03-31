0cf56f1d83b835553f6b37e5517edffcc10e017b
hcyrrr <hhazelcyr@gmail.com>
2024-03-31 15:29:24 +0800

    updated settings methods and added glassdoor sync option

---
+++ src/components/GlassdoorLogo.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { defineComponent } from 'vue';
+
+defineComponent({
+  name: 'GlassdoorLogo',
+});
+</script>
+<template>
+	<svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 32 32">
+		<path fill="#0caa41" d="M22.859 27.427H4.573A4.571 4.571 0 0 0 9.141 32h13.714a4.572 4.572 0 0 0 4.573-4.573V8.656a.165.165 0 0 0-.167-.167h-4.24a.163.163 0 0 0-.161.167v18.776zm0-27.427a4.571 4.571 0 0 1 4.568 4.573H9.146v18.771a.171.171 0 0 1-.167.167h-4.24a.168.168 0 0 1-.167-.167V4.573A4.571 4.571 0 0 1 9.14 0z" />
+	</svg>
+</template>
